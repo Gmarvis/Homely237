bab222229774786808651a11a556bbd95f67e3c1
gmarvis <sgmarvis@gmail.com>
2024-01-30 21:22:57 +0100

    added delete image function

--- src/components/molucles/ProductImageCard.tsx
+++ src/components/molucles/ProductImageCard.tsx
@@ -28,7 +28,7 @@ const ProductImageCard = ({ image, children }: ImageCardProps) => {
       className="w-[100px] mobile:max-sm:w-[22vw]  h-[100px] mobile:max-sm:h-[22vw] [bg-white100px] shadow-md flex items-end flex-col p-2 relative"
     >
       <button
-        className="flex  self-end"
+        className="flex  self-end bg-white rounded-full"
         onClick={() => setShowImageMenu((prev) => !prev)}
       >
         <HiDotsHorizontal size={20} />
@@ -40,9 +40,14 @@ const ProductImageCard = ({ image, children }: ImageCardProps) => {
         />
       )}
       {showMenu && (
-        <div className="absolute top-6 shadow-md bg-white p-2 w-full z-50">
+        <motion.div
+          initial={{ opacity: 0, translateY: -20 }}
+          animate={{ opacity: 1, translateY: 0 }}
+          transition={{ duration: 0.3 }}
+          className="absolute top-6 shadow-md bg-white mt-2 rounded-md p-2 w-full z-50"
+        >
           {children}
-        </div>
+        </motion.div>
       )}
     </motion.div>
   );
