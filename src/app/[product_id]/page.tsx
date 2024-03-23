f6ab89eb453a341167f3b311b75ad0e47abee40d
gmarvis <sgmarvis@gmail.com>
2024-03-23 13:41:37 +0100

    get product by product id

--- src/app/[product_id]/page.tsx
+++ src/app/[product_id]/page.tsx
@@ -2,29 +2,22 @@ import Footer from "@/components/organisms/Footer";
 import NavBar from "@/components/organisms/NavBar";
 import ServiceCardSkeleton from "@/components/organisms/SkeletonLoaders/ServiceCardSkeleton";
 import Head from "next/head";
-import React from "react";
+import ServiceDetails from "./_components/ServiceDetails";
 
-const page = () => {
+const Page = () => {
   return (
     <main>
       <Head>
         <title>servives profile</title>
       </Head>
       <NavBar onDashBoard={false} />
-      <div className="pt-20 px-24 ">
-        <div className="header flex justify-between items-center ">
-          <h2>Product details</h2>
-          <button className="bg-secondrytheme px-3 py-1">Book Service</button>
-        </div>
-
-        <ServiceCardSkeleton />
-      </div>
-      <Footer className="" />
+      <ServiceDetails />
+      {/* <Footer className="" /> */}
     </main>
   );
 };
 
-export default page;
+export default Page;
 
 // >>>>>>TODO<<<<<<
 // >>>>>>Design the details page
