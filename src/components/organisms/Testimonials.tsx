91cb6516963d457e77b401865dabe8322468dfc7
gmarvis <sgmarvis@gmail.com>
2024-04-23 10:40:15 +0100

    adjusted styles

--- src/components/organisms/Testimonials.tsx
+++ src/components/organisms/Testimonials.tsx
@@ -2,24 +2,24 @@ const Testimonils = () => {
   const testimonials = [
     {
       avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
-      name: "Martin escobar",
-      title: "Founder of meta",
+      name: "Sarah K",
+      title: "high school teacher",
       quote:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et est hendrerit, porta nunc vitae.",
+        "I used to spend hours calling around and comparing prices for simple things like appliance repair.With Homygig, I can find qualified professionals in minutes and book appointments easily",
     },
     {
       avatar: "https://randomuser.me/api/portraits/men/46.jpg",
-      name: "Simon andrew",
+      name: "David",
       title: "Software engineer",
       quote:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+        "The technicians I've booked have all been professional, reliable, and trustworthy.  They fixed my plumbing issue quickly and efficiently, and the pricing was fair.  I highly recommend Homygig!",
     },
     {
       avatar: "https://randomuser.me/api/portraits/men/86.jpg",
-      name: "Micheal worin",
-      title: "Product designer",
+      name: "Jean Perez N",
+      title: "Banker",
       quote:
-        "There is no one who loves pain itself, who seeks after it and wants to have it, simply because.",
+        "whatever service i need, I can find them all on Homygig.  The variety of services offered is fantastic, and it saves me the trouble of searching for different companies for each job",
     },
   ];
 
@@ -59,7 +59,7 @@ const Testimonils = () => {
                       {item.quote}
                     </p>
                   </blockquote>
-                  <div className="flex items-center gap-x-4 p-4 mt-6 bg-indigo-50">
+                  <div className="flex items-center gap-x-4 p-4 mt-6 ">
                     <img
                       src={item.avatar}
                       className="w-16 h-16 rounded-full border-2 shadow-md"
