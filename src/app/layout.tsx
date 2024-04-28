e844d629b1008dcee73621e3aba2ad18f983b732
gmarvis <sgmarvis@gmail.com>
2024-04-28 04:48:30 +0100

    working on the pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,34 +6,40 @@ import "react-toastify/dist/ReactToastify.min.css";
 import ThemeProviders from "@/context/themeContext";
 import VerifyUser from "./auth/(verifications)/VerifyUser";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { Head } from "next/document";
 
 export const metadata: Metadata = {
-  title: "homygig",
-  description: "the best home service provission listing application",
+    title: "homygig",
+    description: "the best home service provission listing application",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/* {
+    return (
+        <html lang="en">
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <meta name="theme-color" content="#fff" />
+            </Head>
+            {/* {
           >>>>>>>>>>>>>>THE APP THEME PROVIDER IS NO WORKING PROPERLY
           >>>>>>>>>>>>>>CUSTOMIZED COLORS WON'T CHANGE AS THEME MODE CHANGE
           } */}
-      {/* <ThemeProviders> */}
-      <EdgeStoreProvider>
-        <VerifyUser>
-          <body className="">
-            <ToastContainer />
-            {children}
-          </body>
-        </VerifyUser>
-      </EdgeStoreProvider>
+            {/* <ThemeProviders> */}
+            <EdgeStoreProvider>
+                <VerifyUser>
+                    <body className="">
+                        <ToastContainer />
+                        {children}
+                    </body>
+                </VerifyUser>
+            </EdgeStoreProvider>
 
-      {/* </ThemeProviders> */}
-    </html>
-  );
+            {/* </ThemeProviders> */}
+        </html>
+    );
 }
--- src/components/molucles/Avatar.tsx
+++ src/components/molucles/Avatar.tsx
@@ -1,39 +1,40 @@
 "use client";
-import Avatar from "react-avatar";
+// import Avatar from "react-avatar";
 
 import React from "react";
+import Avatar from "react-avatar";
 type AvatarProps = {
-  user: User;
-  size: number;
-  onClick?: () => void;
+    user: User;
+    size: number;
+    onClick?: () => void;
 };
 
 const ProfileAvatar = ({ user, size, onClick }: AvatarProps) => {
-  if (!user.image) {
+    if (!user.image) {
+        return (
+            <button onClick={onClick}>
+                <Avatar
+                    name={user.name}
+                    size={(size * 10) as unknown as string}
+                    round={true}
+                />
+            </button>
+        );
+    }
     return (
-      <button onClick={onClick}>
-        <Avatar
-          name={user.name}
-          size={(size * 10) as unknown as string}
-          round={true}
-        />
-      </button>
+        <button
+            style={{
+                backgroundImage: `url(${user.image})`,
+                width: `${size * 10}px`,
+                height: `${size * 10}px`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                objectFit: "fill",
+                borderRadius: "50%",
+            }}
+            onClick={onClick}
+        ></button>
     );
-  }
-  return (
-    <button
-      style={{
-        backgroundImage: `url(${user.image})`,
-        width: `${size * 10}px`,
-        height: `${size * 10}px`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        objectFit: "fill",
-        borderRadius: "50%",
-      }}
-      onClick={onClick}
-    ></button>
-  );
 };
 
 export default ProfileAvatar;
