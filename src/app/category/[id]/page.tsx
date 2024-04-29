1132b3c75705f5f87f8e11e8f8487b79104b07c5
gmarvis <sgmarvis@gmail.com>
2024-04-29 16:58:20 +0100

    list by category

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -2,12 +2,16 @@
 import Footer from "@/components/organisms/Footer";
 import NavBar from "@/components/organisms/NavBar";
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import { randomArrayWithinRange } from "./helperFuntions";
+import { randomArrayWithinRange } from "../helperFuntions";
 import Testimonils from "@/components/organisms/Testimonials";
 import SellWithUs from "@/components/organisms/CTA/SellWithUs";
 import ServiceGrid from "@/components/organisms/ServiceGrid";
+
+import { useParams } from "next/navigation";
+import { getServiceByCategoryID } from "@/utils/queries";
 const Page = () => {
+    const [category, setCategory] = useState<Category>();
+    const [services, setServices] = useState<Service[]>([]);
     const [gridImages, setGridImages] = useState<string[]>([
         "https://i.pinimg.com/736x/5c/6a/b6/5c6ab6f93e9d5c83a773698c8ddf25ff.jpg",
         "https://i.pinimg.com/564x/26/fe/04/26fe04dfda1ff7a980a6e8d69fb43151.jpg",
@@ -31,35 +35,49 @@ const Page = () => {
     }, 6000);
     clearInterval(interval);
 
+    const category_id = useParams().id;
+
+    useEffect(() => {
+        getServiceByCategoryID(category_id as unknown as string).then((res) => {
+            setServices(res);
+        });
+    }, []);
+
     return (
         <>
             <div className="min-h-screen">
                 <NavBar onDashBoard={false} />
-                <div className=" h-[80vh] pt-16 flex px-24 mobile:max-sm:px-5 items-center justify-between bg-gradient-to-r  bg-primarytheme">
+                <div className=" h-[80vh] pt-16 flex px-24 mobile:max-sm:px-5 items-center justify-between bg-gradient-to-r bg-primarytheme">
                     <h3 className="text-white text-5xl font-extrabold ">
                         Get the best <br /> Plumbing services
                     </h3>
 
-                    <div className="shapes grid grid-cols-2 gap-4">
+                    <div className="shapes grid grid-cols-2 gap-4 ">
                         {gridImages.map((image, i) => (
                             <div
                                 key={i}
                                 style={{
                                     backgroundImage: `url(${image})`,
                                 }}
-                                className={`bg-white shadow-sm w-64  bg-center bg-cover h-48 mobile:max-sm:hidden sm:max-md:h-16 sm:max-md:w-16 transition-all duration-300 ${
-                                    i === 0 ? "rounded-tl-full " : ""
-                                } ${i === 3 ? "rounded-br-full" : ""} `}
+                                className={`bg-white shadow-sm w-64  bg-center bg-cover h-52 hover:scale-105 mobile:max-sm:hidden sm:max-md:h-16 sm:max-md:w-16 transition-all duration-300 ${
+                                    i === 0
+                                        ? "rounded-tl-full rounded-bl-full "
+                                        : " rounded-md"
+                                } ${
+                                    i === 3
+                                        ? "rounded-br-full rounded-tr-full"
+                                        : ""
+                                } `}
                             ></div>
                         ))}
                     </div>
                 </div>
                 <div></div>
+                <ServiceGrid title="plumbing services" services={services} />
+                <Testimonils />
+                <SellWithUs />
+                <Footer />
             </div>
-            <ServiceGrid title="plumbing services" services={[]} />
-            <Testimonils />
-            <SellWithUs />
-            <Footer />
         </>
     );
 };
