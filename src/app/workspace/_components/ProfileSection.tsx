45dfbc14a18140ae05d2a853dcac754373ea2915
gmarvis <sgmarvis@gmail.com>
2024-05-17 01:28:40 +0100

    create service provider form

---
+++ src/app/workspace/_components/ProfileSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa6";
+
+const ProfileSection = () => {
+    return (
+        <div className="h-40 bg-primarytheme p-2 flex ">
+            <div
+                className="w-40 h-full bg-white relative"
+                style={{
+                    backgroundImage: `url(${
+                        // user?.image ||
+                        "https://i.pinimg.com/564x/9e/83/75/9e837528f01cf3f42119c5aeeed1b336.jpg"
+                    })`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    objectFit: "fill",
+                    backgroundRepeat: "no-repeat",
+                    // borderRadius: "50%",
+                }}
+            >
+                <button className="bg-primarytheme w-10 h-10 rounded-full flex justify-center items-center shadow-sm shadow-white text-white absolute bottom-2 left-[140px]">
+                    <FaPlus size={24} />
+                </button>
+            </div>
+            <div className="w-[60%] flex justify-center items-center">
+                <h2 className="text-center text-gray-600 text-3xl mobile:max-sm:text-xl font-semibold">
+                    Service Provider Profile
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileSection;
--- src/app/workspace/create/page.tsx
+++ src/app/workspace/create/page.tsx
@@ -1,10 +1,54 @@
+"use client";
 import NavBar from "@/components/organisms/NavBar";
 import React from "react";
+import ProfileSection from "../_components/ProfileSection";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 
 const page = () => {
     return (
         <div>
             <NavBar />
+            <div className="pt-[58px] px-24 w-full  mobile:max-sm:px-2 pb-10">
+                <div className="w-full flex justify-center">
+                    <div className=" max-w-2xl mobile:max-sm:max-w-full mobile:max-sm:w-full w-full space-y-3">
+                        <ProfileSection />
+                        <form>
+                            <Label className=" text-gray-600 text-xs">
+                                Name
+                            </Label>
+                            <Input placeholder="name" />
+                            <Label className=" text-gray-600 text-xs">
+                                Phone number (whatsapp)
+                            </Label>
+                            <Input placeholder="Phone number" type="number" />
+                            <Label className=" text-gray-600 text-xs">
+                                Location
+                            </Label>
+                            <Input placeholder="location" />
+                            <Label className=" text-gray-600 text-xs">
+                                National ID card front image
+                            </Label>
+                            <Input type="file" />
+                            <Label className=" text-gray-600 text-xs">
+                                National ID card back image
+                            </Label>
+                            <Input type="file" />
+                            <Label className=" text-gray-600 text-xs">
+                                Bio (say something about yourself)
+                            </Label>
+
+                            <Textarea />
+
+                            <Button className="bg-primarytheme mt-5">
+                                Procceed
+                            </Button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
