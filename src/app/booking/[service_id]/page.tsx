a7ed63f977392e72c7bccfbd0895e036aede82b4
gmarvis <sgmarvis@gmail.com>
2024-06-15 08:18:23 +0100

    finished with apoinments

--- src/app/booking/[service_id]/page.tsx
+++ src/app/booking/[service_id]/page.tsx
@@ -4,105 +4,118 @@ import { useParams, usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import * as Queries from "@/utils/queries";
-// react icons
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import DatePicker from "./_components/DatePicker";
 import useUserStore from "@/store/userStore";
+import useLocationStore from "@/store/locationStore";
 
 const Page = () => {
-    const [service, setService] = useState<Service | null>();
-    const [location, setLocation] = useState("");
-    const [selectedDate, setSelectedDate] = useState<Date>();
-    const [description, setDescription] = useState("");
-    const { user } = useUserStore();
-    // get Service Id form the params
-    const serviceId = usePathname().split("/")[2];
-    console.log("serviceId", serviceId);
-    const service_id = useParams().id;
+	const [service, setService] = useState<Service | null>();
+	const [location, setLocation] = useState("");
+	const [selectedDate, setSelectedDate] = useState<Date>();
+	const [description, setDescription] = useState("");
+	const [phoneNumber, setPhomeNumber] = useState("")
+	const { user } = useUserStore();
+	const serviceId = usePathname().split("/")[2];
+	// console.log("serviceId", serviceId);
 
-    useEffect(() => {
-        Queries.getServiceByServiceID(serviceId).then((res: Service) => {
-            if (res.id) {
-                setService(res);
-                console.log(res);
-            }
-        });
-    }, []);
 
-    const handleBooking = (e: { preventDefault: () => void }) => {
-        e.preventDefault();
-        const bookingDetails = {
-            user_id: user.id,
-            provider_id: service?.user?.id,
-            product_id: serviceId,
-            location,
-            selectedDate,
-            description,
-        };
+	const { currentLocation } = useLocationStore();
 
-        console.log("bookingDetails", bookingDetails);
-    };
+	useEffect(() => {
+		Queries.getServiceByServiceID(serviceId).then((res: Service) => {
+			if (res.id) {
+				setService(res);
+				console.log(res);
+			}
+		});
+	}, []);
 
-    return (
-        <div>
-            <NavBar hideSearchBar={true} onDashBoard={false} />
-            <div className="flex justify-center items-center mobile:max-sm:justify-start  mobile:max-sm:items-start   pt-[60px] h-[100vh] w-full relative">
-                <div className="w-[40rem] flex flex-col justify-center mobile:max-sm:w-full h-[35rem]  p-2 bg-white absolute sm:shadow-md">
-                    <div className="max-w-md self-center text-center mb-10">
-                        <h1 className="text-center font-bold">
-                            Ready to Book your service?
-                        </h1>
-                        <p className="text-xs text-gray-700">
-                            lets make sure the provider get what you need by
-                            providing details in the form bellow
-                        </p>
-                    </div>
+	const handleBooking = (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+		const bookingDetails = {
+			user_id: user.id,
+			provider_id: service?.user?.id,
+			product_id: serviceId,
+			location_detials: location,
+			date:  selectedDate,
+			city: currentLocation.city,
+			locality: currentLocation.locality,
+			description,
+			phone_number: phoneNumber
+		};
 
-                    <form
-                        onSubmit={handleBooking}
-                        className="flex flex-col gap-4 space-y-3"
-                    >
-                        <div>
-                            <Label>Discribe your location</Label>
-                            <Input
-                                onChange={(e) => setLocation(e.target.value)}
-                            />
-                        </div>
-                        <div className="flex flex-col">
-                            <Label>Enter date</Label>
-                            <DatePicker onSelectDate={setSelectedDate} />
-                        </div>
-                        <div>
-                            <Label className="flex py-1 justify-between">
-                                <p>
-                                    Describe what you need to help the provider
-                                    come prepared
-                                </p>
-                                <span
-                                    className={`${
-                                        description.length < 600
-                                            ? "text-red-600"
-                                            : "text-green-600"
-                                    }`}
-                                >
-                                    {description.length || 0}/ 600
-                                </span>
-                            </Label>
-                            <Textarea
-                                className="text-gray-800"
-                                onChange={(e) => setDescription(e.target.value)}
-                            />
-                        </div>
+		Queries.creatApointment(bookingDetails)
+		console.log("bookingDetails", bookingDetails);
+	};
 
-                        <Button className="bg-primarytheme">Book Now</Button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<NavBar hideSearchBar={true} onDashBoard={false} />
+			<div className="flex justify-center items-center mobile:max-sm:justify-start  mobile:max-sm:items-start   pt-[60px] h-[100vh] w-full relative">
+				<div className="w-[40rem] flex flex-col justify-center mobile:max-sm:w-full h-auto  p-2 bg-white absolute sm:shadow-md">
+					<div className="max-w-md self-center text-center mb-10">
+						<h1 className="text-center text-3xl font-bold">
+							Ready to Book your service?
+						</h1>
+						<p className="text-xs text-gray-700">
+							lets make sure the provider get what you need by providing details
+							in the form bellow
+						</p>
+					</div>
+
+					<form
+						onSubmit={handleBooking}
+						className="flex flex-col gap-4 space-y-3"
+					>
+						<div>
+							<p> City: {currentLocation.city}</p>
+							<p> Locality: {currentLocation.locality}</p>
+						</div>
+						<div className="flex flex-col">
+							<Label>Enter date</Label>
+							<DatePicker onSelectDate={setSelectedDate} />
+						</div>
+						<div>
+							<Label>WhatsApp Number</Label>
+							<Input
+								type="number"
+								placeholder="670000000"
+								onChange={(e) => setPhomeNumber(e.target.value)}
+							/>
+						</div>
+
+						<div>
+							<Label>Discribe your location</Label>
+							<Input onChange={(e) => setLocation(e.target.value)} />
+						</div>
+
+						<div>
+							<Label className="flex py-1 justify-between">
+								<p>Describe what you need to help the provider come prepared</p>
+								<span
+									className={`${
+										description.length < 600 ? "text-red-600" : "text-green-600"
+									}`}
+								>
+									{description.length || 0}/ 600
+								</span>
+							</Label>
+							<Textarea
+								className="text-gray-800"
+								onChange={(e) => setDescription(e.target.value)}
+							/>
+						</div>
+
+						<Button className="bg-primarytheme">Book Now</Button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Page;
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -4,25 +4,25 @@ import ApiCall from "./httpClient";
 const apiCall = new ApiCall();
 
 type SigUpType = {
-    name: string;
-    email: string;
-    password: string;
+	name: string;
+	email: string;
+	password: string;
 };
 
 // SIGN UP
 export const SIGNUP = (newUser: SigUpType) => {
-    return apiCall.POST(SERVER_URL + "/users/signup", newUser);
+	return apiCall.POST(SERVER_URL + "/users/signup", newUser);
 };
 
 type LoginType = {
-    email: string;
-    password: string;
+	email: string;
+	password: string;
 };
 
 // LOGIN
 export const LOGIN = (returningUser: LoginType) => {
-    // console.log(returningUser);
-    return apiCall.POST(SERVER_URL + "/users/login", returningUser);
+	// console.log(returningUser);
+	return apiCall.POST(SERVER_URL + "/users/login", returningUser);
 };
 
 // GET ALL PRODUCT CATEGORIES
@@ -30,82 +30,93 @@ export const getAllCategories = () => apiCall.GET(SERVER_URL + "/categories");
 
 // CREATE SERVICE(PRODUCT)
 type SevType = {
-    user_id: string;
-    name: string;
-    category_name: string;
-    images: [];
-    product_image: string;
-    price: string;
-    category_id: string;
-    description: string;
+	user_id: string;
+	name: string;
+	category_name: string;
+	images: [];
+	product_image: string;
+	price: string;
+	category_id: string;
+	description: string;
 };
 export const createService = (serviceDetails: SevType) => {
-    return apiCall.POST(SERVER_URL + "/products", serviceDetails);
+	return apiCall.POST(SERVER_URL + "/products", serviceDetails);
 };
 
 // GET ALL SERVICES
 export const getAllServices = () => {
-    return apiCall.GET(SERVER_URL + "/products");
+	return apiCall.GET(SERVER_URL + "/products");
 };
 
 // GET SERVICE(PRODUCT) BY THE USER_ID
 export const getServiceByUserID = (user_id: string) => {
-    return apiCall.GET(SERVER_URL + `/products/user/${user_id}`);
+	return apiCall.GET(SERVER_URL + `/products/user/${user_id}`);
 };
 
 // GET SINGLE PRODUCT BY PRODUCT ID
 export const getServiceByServiceID = (product_id: string): Promise<any> => {
-    try {
-        return apiCall.GET(`${SERVER_URL}/products/${product_id}`);
-    } catch (error: any) {
-        return error;
-    }
+	try {
+		return apiCall.GET(`${SERVER_URL}/products/${product_id}`);
+	} catch (error: any) {
+		return error;
+	}
 };
 
 // DELETE SERVICE
 export const deleteService = (service_id: string): any => {
-    try {
-        return apiCall.DELETE(SERVER_URL + `/products/${service_id}`);
-    } catch (error: any) {
-        console.log(error);
-    }
+	try {
+		return apiCall.DELETE(SERVER_URL + `/products/${service_id}`);
+	} catch (error: any) {
+		console.log(error);
+	}
 };
 
 // GET SINGLE PRODUCT BY CATEGORY ID
 export const getServiceByCategoryID = (category_id: string): Promise<any> => {
-    try {
-        return apiCall.GET(`${SERVER_URL}/products/category/${category_id}`);
-    } catch (error: any) {
-        return error;
-    }
+	try {
+		return apiCall.GET(`${SERVER_URL}/products/category/${category_id}`);
+	} catch (error: any) {
+		return error;
+	}
 };
 
 // UPDATE PRODRUCT
 export const updateService = (id: string, newData: any): any => {
-    try {
-        return apiCall.PUT(`${SERVER_URL}/products/${id}`, newData);
-    } catch (error) {
-        return error;
-    }
+	try {
+		return apiCall.PUT(`${SERVER_URL}/products/${id}`, newData);
+	} catch (error) {
+		return error;
+	}
 };
 
 export const getCurrentLocation = async (
-    latitude: number,
-    longitude: number
+	latitude: number,
+	longitude: number
 ): Promise<CurrentLoacation | undefined> => {
-    try {
-        const data = await apiCall.GET(
-            `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
-        );
-        if (data) {
-            return {
-                city: data.city,
-                continent: data.continent,
-                continentCode: data.continentCode,
-                countryCode: data.countryCode,
-                countryName: data.countryName,
-                locality: data.locality,
-            };
-        }
-    } catch (error) {}
+	try {
+		const data = await apiCall.GET(
+			`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+		);
+		if (data) {
+			return {
+				city: data.city,
+				continent: data.continent,
+				continentCode: data.continentCode,
+				countryCode: data.countryCode,
+				countryName: data.countryName,
+				locality: data.locality,
+			};
+		}
+	} catch (error) {}
+};
+
+// Create appointments
+export const creatApointment = async (aptmData: {}) => {
+	try {
+		const data = await apiCall.POST(`${SERVER_URL}/appointments`, aptmData);
+        console.log(data)
+        return data
+	} catch (error) {
+		console.log(error);
+	}
 };
