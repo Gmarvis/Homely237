d044a66a7cd72cb7ff0d5e931f69ea3057fa1a98
gmarvis <sgmarvis@gmail.com>
2024-06-17 11:28:01 +0100

    prettier config

--- src/app/[product_id]/_components/ServiceDetails.tsx
+++ src/app/[product_id]/_components/ServiceDetails.tsx
@@ -6,160 +6,140 @@ import { useEffect } from "react";
 import Image from "next/image";
 import ServiceCard from "@/components/organisms/cards/ServiceCard";
 import { useRouter } from "next/navigation";
-import ServiceCardSkeleton from "@/components/organisms/SkeletonLoaders/ServiceCardSkeleton";
-import TextSkeleton from "@/components/organisms/SkeletonLoaders/TextSkeleton";
 import DetailsSkeleton from "./DetailsSkeleton";
-
+import { ImWhatsapp } from "react-icons/im";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import Link from "next/link";
+import MasonryList from "@/components/organisms/MasonryList";
 const ServiceDetails = () => {
-    const [service, setService] = useState<Service | null>(null);
-    const [relatedProducts, SetRelatedProducts] = useState<Service[] | null>(
-        null
-    );
-    const [view, setView] = useState("");
+	const [service, setService] = useState<Service | null>(null);
+	const [relatedProducts, SetRelatedProducts] = useState<Service[] | null>(
+		null
+	);
+	const [view, setView] = useState("");
 
-    const router = useRouter();
+	const router = useRouter();
 
-    // get Service Id form the params
-    const serviceId = usePathname().slice(1);
+	// get Service Id form the params
+	const serviceId = usePathname().slice(1);
 
-    useEffect(() => {
-        Queries.getServiceByServiceID(serviceId).then((res) => {
-            if (res.id) {
-                setService(res);
-                setView(res.product_image);
-                Queries.getServiceByCategoryID(res.category_id).then((res) => {
-                    if (!res.error) SetRelatedProducts(res);
-                    // console.log("related products", res);
-                });
-                // console.log(res);
-            }
-        });
-    }, [serviceId]);
-    <h2>{service?.name}</h2>;
+	useEffect(() => {
+		Queries.getServiceByServiceID(serviceId).then((res) => {
+			if (res.id) {
+				setService(res);
+				setView(res.product_image);
+				Queries.getServiceByCategoryID(res.category_id).then((res) => {
+					if (!res.error) SetRelatedProducts(res);
+					// console.log("related products", res);
+				});
+				// console.log(res);
+			}
+		});
+	}, [serviceId]);
+	<h2>{service?.name}</h2>;
 
-    return (
-        <div className="pt-[55px] mobile:max-sm:pt-[60px] mobile:max-sm:px-2 px-24 bigScreen:px-80 gap-5 mobile:max-sm:gap-1">
-            <div className="flex flex-col gap-2">
-                <div className="flex pt-4 mobile:max-sm:flex-col justify-between items-center gap-10 shadow-sm p-2 mobile:max-sm:p-0">
-                    <div className="iamges mobile:max-sm:w-[100%] mobile:max-sm:h-[300px] w-[700px] h-[400px]  bg-gray-200">
-                        <Image
-                            src={
-                                view ||
-                                "https://i.pinimg.com/564x/9d/90/33/9d903364960a75dcb7f5ea8af91fa44f.jpg"
-                            }
-                            alt=""
-                            bg-red-600
-                            width={400}
-                            height={400}
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    {!service ? (
-                        <DetailsSkeleton />
-                    ) : (
-                        <div className="detials w-[100%] flex flex-col gap-5 p-2 ">
-                            <div>
-                                <h3 className="font-bold text-gray-700 text-2xl">
-                                    {`${service?.name
-                                        .charAt(0)
-                                        .toUpperCase()}${service?.name.slice(
-                                        1
-                                    )}`}
-                                </h3>
-                                <p className="text-slate-600 text-sm">
-                                    {service?.category_name} services
-                                </p>
-                            </div>
-                            <div className="">
-                                <span className="bg-secondrytheme p-1 px-3  text-white">
-                                    {service?.price} CFA per hour
-                                </span>
+	return (
+		<>
+			<div className="pt-[55px] mobile:max-sm:pt-[60px] mobile:max-sm:px-2 px-24 bigScreen:px-80 gap-5 mobile:max-sm:gap-1">
+				<div className="flex flex-col gap-2 h-[80vh] mt-10 rounded-3xl shadow-md border mobile:max-sm:h-full">
+					<div className="flex  h-full mobile:max-sm:flex-col justify-between items-center shadow-sm mobile:max-sm:p-0">
+						<div className="iamges mobile:max-sm:w-[100%] mobile:max-sm:h-[300px] w-full h-full rounded-tl-2xl mobile:max-sm:rounded-b-[0px]  rounded-bl-2xl mobile:max-sm:rounded-2xl bg-gray-200 relative">
+								<button 
+                                onClick={()=> router.back()}
+                                className="absolute left-5 top-5 bg-primarytheme hover:bg-secondrytheme duration-300 p-2 rounded-full text-white">
+									<IoMdArrowRoundBack size={40} />
+								</button>
 
-                                <p className="text-sm text-slate-700 mt-3 h-32 ">
-                                    {service?.description}
-                                </p>
-                            </div>
+							<Image
+								src={
+									view ||
+									"https://i.pinimg.com/564x/9d/90/33/9d903364960a75dcb7f5ea8af91fa44f.jpg"
+								}
+								alt=""
+								bg-red-600
+								width={400}
+								height={400}
+								className="w-full h-full object-cover rounded-tl-2xl  rounded-bl-2xl mobile:max-sm:rounded-b-[0px] mobile:max-sm:rounded-t-2xl"
+							/>
+						</div>
+						{!service ? (
+							<DetailsSkeleton />
+						) : (
+							<div className="detials w-[100%] flex flex-col gap-5 p-5 h-full ">
+								<div className="flex justify-between">
+									<div className="">
+										<h3 className="font-bold text-gray-700 text-2xl">
+											{`${service?.name
+												.charAt(0)
+												.toUpperCase()}${service?.name.slice(1)}`}
+										</h3>
+										<p className="text-slate-600 text-sm">
+											{service?.category_name} services
+										</p>
+									</div>
+									<button
+										className=""
+										onClick={() => alert("conneting to whatsApp")}
+									>
+										<ImWhatsapp size={30} className="text-green-600" />
+									</button>
+								</div>
+								<div className="flex-grow">
+									<span className="bg-secondrytheme p-1 px-3  text-white">
+										{service?.price} CFA per hour
+									</span>
 
-                            <div className="flex gap-2 ">
-                                {service?.images.map((image, i) => (
-                                    <div
-                                        className="w-[60px] h-[60px] hover:cursor-pointer"
-                                        key={i}
-                                        onClick={() => setView(image)}
-                                    >
-                                        <Image
-                                            src={image}
-                                            alt=""
-                                            width={200}
-                                            height={200}
-                                            className="object-cover w-full h-full"
-                                        />
-                                    </div>
-                                ))}
-                            </div>
+									<p className="text-sm text-slate-700 mt-3 h-32 ">
+										{service?.description}
+									</p>
+								</div>
 
-                            <button
-                                onClick={() =>
-                                    router.push(`/booking/${service.id}`)
-                                }
-                                className=" border border-primarytheme text-primarytheme py-2"
-                            >
-                                BOOK AN APPOINTMENT
-                            </button>
-                        </div>
-                    )}
-                </div>
-            </div>
+								<div className="flex gap-2 ">
+									{service?.images.map((image, i) => (
+										<div
+											className="w-[100px] h-[100px] hover:cursor-pointer hover:scale-105 duration-300"
+											key={i}
+											onClick={() => setView(image)}
+										>
+											<Image
+												src={image}
+												alt=""
+												width={200}
+												height={200}
+												className="object-cover w-full h-full"
+											/>
+										</div>
+									))}
+								</div>
 
-            <div className="pt-10">
-                {relatedProducts && (
-                    <h3 className="text-lg font-semibold text-gray-700">
-                        Related Services
-                    </h3>
-                )}
+								<button
+									onClick={() => router.push(`/booking/${service.id}`)}
+									className=" border bg-primarytheme text-white hover:bg-secondrytheme duration-300 py-3 rounded-full"
+								>
+									BOOK AN APPOINTMENT
+								</button>
+							</div>
+						)}
+					</div>
+				</div>
 
-                {!relatedProducts && (
-                    <div className=" grid-cols-4 mobile:max-sm:grid-cols-2 mobile:max-sm:gap-2 mb-5 2xl:px-24 items-center justify-center mobile:max-sm:grid hidden z-0 w-full">
-                        {[1, 2, 3, 4].map((item, i) => (
-                            <ServiceCardSkeleton key={i} />
-                        ))}
-                    </div>
-                )}
+				<div className="pt-10">
+					{relatedProducts && (
+						<h3 className=" text-center text-3xl font-semibold text-gray-700">
+							Related Services
+						</h3>
+					)}
+				</div>
+			</div>
 
-                {!relatedProducts && (
-                    <div className="flex gap-10 pl-3 flex-wrap items-center py-10 mobile:max-sm:hidden z-0 w-full justify-center">
-                        {[1, 2, 3, 4].map((item, i) => (
-                            <ServiceCardSkeleton key={i} />
-                        ))}
-                    </div>
-                )}
-
-                <div className=" grid-cols-4 mobile:max-sm:grid-cols-2 mobile:max-sm:gap-2 mb-5 2xl:px-24 items-center justify-center mobile:max-sm:grid hidden w-full">
-                    {relatedProducts
-                        ?.filter((item) => item.id !== serviceId)
-                        .map((service, i) => (
-                            <ServiceCard
-                                key={i}
-                                service={service}
-                                onClick={() => router.push(`/${service.id}`)}
-                            />
-                        ))}
-                </div>
-
-                <div className="flex gap-10 flex-wrap py-2 mobile:max-sm:hidden   ">
-                    {relatedProducts
-                        ?.filter((item) => item.id !== serviceId)
-                        .map((service, i) => (
-                            <ServiceCard
-                                key={i}
-                                service={service}
-                                onClick={() => router.push(`/${service.id}`)}
-                            />
-                        ))}
-                </div>
-            </div>
-        </div>
-    );
+			{relatedProducts && (
+				<MasonryList
+					services={relatedProducts.filter((item) => item.id !== serviceId)}
+					onClickOpen={(id) => router.push(`/${id}`)}
+				/>
+			)}
+		</>
+	);
 };
 
 export default ServiceDetails;
